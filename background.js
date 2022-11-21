4c8e0402e8c20a30a3f299b0fcffa40a4ea71b6d
Mahsusa_Beg <99585603+mahsusabeg@users.noreply.github.com>
2022-11-21 10:24:01 +0400

    Update background.js

--- background.js
+++ background.js
@@ -1,7 +1,6 @@
 chrome.tabs.onUpdated.addListener(
     function(tabId, changeInfo, tab){
         if (changeInfo.url){
-            // Reset the gain node
             chrome.tabs.sendMessage(
                 tabId,
                 {
@@ -10,7 +9,6 @@ chrome.tabs.onUpdated.addListener(
             );
         }
         else{
-            // Apply previous settings
             chrome.tabs.sendMessage(
                 tabId,
                 {
