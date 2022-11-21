d78b5d6cdc583d4e6865e48fe3182490ecd10488
Mahsusa_Beg <99585603+mahsusabeg@users.noreply.github.com>
2022-11-21 10:23:30 +0400

    Update content.js

--- content.js
+++ content.js
@@ -1,8 +1,6 @@
 const VOLUME_KEY = "volume_key";
 const DEFAULT_VOLUME_PERCENTAGE = 100;
 let currentVolumePercentage;
-
-// Gain Node controls the volume of the Media Stream
 let gainNode;
 
 
@@ -12,29 +10,20 @@ initializeRequestListeners();
 console.log("Now volume: " + currentVolumePercentage);
 
 
-/**
- * Helper Functions
- */
 function initializeCurrentVolumePercentage() {
     currentVolumePercentage = sessionStorage.getItem(VOLUME_KEY);
 
     console.log("Saved volume: " + currentVolumePercentage);
 
-    // if null, then set as default value and store in localstorage
     if (currentVolumePercentage == null) {
         currentVolumePercentage = DEFAULT_VOLUME_PERCENTAGE;
         sessionStorage.setItem(VOLUME_KEY, String(currentVolumePercentage));
     }
-    // Do casting here after checking for null.
     currentVolumePercentage = Number(currentVolumePercentage);
 
     console.log("Updated volume: " + currentVolumePercentage);
 }
-
-
-/**
- * Add a listener to initialize gainNode and adjust volume
- */
+ 
 function initializeRequestListeners() {
     chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
             console.log(request.message);
@@ -71,10 +60,7 @@ function initializeRequestListeners() {
         }
     );
 }
-
-/**
- * Adjust volume
- */
+ 
 function adjustVolume(){
     gainNode = gainNode || firstTimeSetUp();
 
@@ -86,51 +72,29 @@ function adjustVolume(){
         }
     }
 }
-
-/**
- * Resets the VOLUME_KEY of the current tab's sessionStorage to default value.
- */
+ 
 function resetVolume(){
     sessionStorage.setItem(VOLUME_KEY, String(DEFAULT_VOLUME_PERCENTAGE));
     currentVolumePercentage = DEFAULT_VOLUME_PERCENTAGE;
 }
-
-/**
- * Sets up the gain node for the first time.
- * @returns {null|GainNode}
- */
-function firstTimeSetUp() {
-    // Assume there is only one video element on the HTML page, since querySelector returns the first video.
+ 
+function firstTimeSetUp() { 
     const mediaStream = document.querySelector("video");
 
     if (!mediaStream) {
         return null;
     }
-
-    // Get a GainNode so that we can change the gain value upon request.
+ 
     const gainNode = createGainNodeFromAudioContext(mediaStream);
 
     return gainNode;
 }
-
-/**
- * Creates a gain node from the media stream
- * @param mediaStream - a Media Stream class
- * @returns {GainNode} - a Gain Node
- */
+ 
 function createGainNodeFromAudioContext(mediaStream) {
-    // Create an AudioContext.
     const audioContext = new AudioContext();
-    // Create a MediaElementAudioSourceNode and feed the media stream into it.
     const mediaElementAudioSourceNode = audioContext.createMediaElementSource(mediaStream);
-    // Create a GainNode to adjust volume.
     const gainNode = audioContext.createGain();
-
-    // Reset the default gain to 1 (Default is 1 and range is (-inf, inf)).
     gainNode.gain.value = 1;
-
-    // Connect the MediaElementAudioSourceNode to the gainNode and the gainNode to the audio destination
-    // (the final destination of the audio in the context, i.e. a physical speaker or headset etc.).
     mediaElementAudioSourceNode.connect(gainNode);
     gainNode.connect(audioContext.destination);
 
